85aa8261fec90f44378bd0ff9bda0eaf24077d41
Agbata Dickson Ukolojo <100839911+Ukolojoaud@users.noreply.github.com>
2025-03-28 08:22:57 +0100

    Add files via upload
    Changes

---
+++ SQL Works.sql
@@ -0,0 +1,263 @@
+USE classicmodels;
+
+CREATE TABLE coy_finance(
+Date DATE NOT NULL
+,Company VARCHAR(29) NOT NULL
+,Product VARCHAR(42) NOT NULL
+,Revenue INTEGER NOT NULL
+,COGS VARCHAR(18) NOT NULL
+,Operating_Expenses VARCHAR(18) NOT NULL
+,Marketing_Expenses VARCHAR(18) NOT NULL
+,RD_Expenses VARCHAR(18) NOT NULL
+,Gross_Profit VARCHAR(18) NOT NULL
+,Net_Profit VARCHAR(19) NOT NULL
+);
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-01-31',
+'Vasquez Ltd',
+'strategize virtual experiences',
+74530,
+42899.145769539915,
+11465.542256589088,
+11047.25442216059,
+5153.388081809797,
+31630.854230460085,
+3964.669469900611
+);
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-02-28',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,168623,
+'74557.10122303382',
+'24845.603359980174',
+'19021.188102710905',
+'15428.10000',
+'3332.0588',
+'2310.5888');
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-01-31',
+'Vasquez Ltd',
+'strategize virtual experiences',
+74530,
+42899.145769539915,
+11465.542256589088,
+11047.25442216059,
+5153.388081809797,
+31630.854230460085,
+3964.669469900611
+);
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-03-25',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,148623,
+'54557.10122303382',
+'14845.603359980174',
+'18021.188102710905',
+'15228.10000',
+'3432.0588',
+'4310.5888');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-03-25',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,208623,
+'94557.10122303382',
+'16845.603359980174',
+'18300.188102710905',
+'17228.10000',
+'4432.0588',
+'5310.5888');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-03-25',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,308623,
+'14557.10122303382',
+'15845.603359980174',
+'16300.188102710905',
+'14228.10030',
+'4532.05998',
+'6310.5898');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-04-28',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,268623,
+'14857.10122303382',
+'15945.603359980174',
+'14300.188102710905',
+'15228.10030',
+'4432.05998',
+'6310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-05-30',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,468623,
+'19857.10122303382',
+'16645.603359980174',
+'13300.188102710905',
+'13228.10030',
+'5432.05998',
+'8310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-06-30',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,368623,
+'18857.10122303382',
+'17645.603359980174',
+'14300.188102710905',
+'13528.10030',
+'6432.05998',
+'10310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-06-30',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,2668623,
+'18557.10122303382',
+'14645.603359980174',
+'12300.188102710905',
+'11528.10030',
+'6632.05998',
+'11310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-07-27',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,5668623,
+'19555.10122303382',
+'15645.603359980174',
+'14300.188102710905',
+'14528.10030',
+'8632.05998',
+'13310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-08-28',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,5068623,
+'20555.10122303382',
+'17645.603359980174',
+'15300.188102710905',
+'16528.10030',
+'6000.05498',
+'8310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-08-28',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,4068623,
+'21000.10122303382',
+'14645.603359980174',
+'16300.188102710905',
+'14528.10030',
+'8000.05498',
+'9310.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-09-24',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,4568623,
+'22000.10122303382',
+'18645.603359980174',
+'163100.188102710905',
+'18000.10030',
+'8311.05398',
+'7533.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-10-24',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,3868623,
+'26000.10122303382',
+'17645.603359980174',
+'163100.188102710905',
+'14000.10030',
+'13311.05398',
+'6533.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-10-24',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,2868623,
+'22000.10122303382',
+'13645.603359980174',
+'13100.188102710905',
+'11000.10030',
+'7311.05398',
+'4533.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-11-29',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,2068623,
+'15000.10122303382',
+'1645.603359980174',
+'1100.188102710905',
+'1000.10030',
+'8311.05398',
+'4533.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2019-11-29',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,568623,
+'180000.10122303382',
+'23645.603359980174',
+'15100.188102710905',
+'19000.10030',
+'250311.05398',
+'145033.500');
+
+INSERT INTO coy_finance(
+Date,Company,Product,Revenue,COGS,Operating_Expenses,Marketing_Expenses,RD_Expenses,Gross_Profit,Net_Profit) 
+VALUES('2020-02-29',
+'Vasquez Ltd',
+'strategize virtual experiences'
+,68623,
+'18000.10122303382',
+'2345.603359980174',
+'10100.188102710905',
+'9000.10030',
+'20311.05398',
+'125033.500');
+
+SELECT * FROM coy_finance;
